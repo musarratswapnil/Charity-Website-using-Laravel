cc291e54b2e0239628e7d1d81d3dfcf28ff3dcfc
Swapnil <shaeer.musarrat@gmail.com>
2024-03-28 12:57:45 +0600

    admin panel dynamic dashboard page created

--- app/Http/Controllers/Admin/AdminHomeController.php
+++ app/Http/Controllers/Admin/AdminHomeController.php
@@ -9,6 +9,6 @@ class AdminHomeController extends Controller
 {
     public function index()
     {
-        return view('admin.app');
+        return view('admin.home');
     }
 }
--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
 
-    <link rel="icon" type="image/png" href="uploads/favicon.png">
+    <link rel="icon" type="image/png" href="{{ asset('uploads/favicon.png') }}">
 
     <title>Admin Panel</title>
 
@@ -64,7 +64,7 @@
                     <a href="" target="_blank" class="btn btn-warning text-primary">Front End</a>
                 </li>
                 <li class="dropdown"><a href="#" data-toggle="dropdown" class="nav-link dropdown-toggle nav-link-lg nav-link-user">
-                    <img alt="image" src="uploads/user.jpg" class="rounded-circle mr-1">
+                    <img alt="image" src="{{ asset('uploads/user.jpg') }}" class="rounded-circle mr-1">
                     <div class="d-sm-none d-lg-inline-block text-primary">John Doe</div></a>
                     <div class="dropdown-menu dropdown-menu-right">
                         <a href="profile.html" class="dropdown-item has-icon text-primary">
@@ -116,55 +116,10 @@
         <div class="main-content">
             <section class="section">
                 <div class="section-header">
-                    <h1 class="text-primary">Dashboard</h1>
+                    <h1 class="text-primary">@yield('heading')</h1>
                 </div>
-                <div class="row">
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="card card-statistic-1">
-                            <div class="card-icon bg-primary">
-                                <i class="far fa-user"></i>
-                            </div>
-                            <div class="card-wrap">
-                                <div class="card-header" >
-                                    <h4 class="text-primary">Total News Categories</h4>
-                                </div>
-                                <div class="card-body">
-                                    12
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="card card-statistic-1">
-                            <div class="card-icon bg-danger">
-                                <i class="fas fa-book-open"></i>
-                            </div>
-                            <div class="card-wrap">
-                                <div class="card-header">
-                                    <h4 class="text-primary">Total News</h4>
-                                </div>
-                                <div class="card-body">
-                                    122
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-lg-4 col-md-6 col-sm-6 col-12">
-                        <div class="card card-statistic-1">
-                            <div class="card-icon bg-warning">
-                                <i class="fas fa-bullhorn"></i>
-                            </div>
-                            <div class="card-wrap">
-                                <div class="card-header">
-                                    <h4 class="text-primary">Total Users</h4>
-                                </div>
-                                <div class="card-body">
-                                    45
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
+                @yield('main_content')
             </section>
         </div>
 
