8d0b4a2ad06d7c92646d5bf1b6e6f8fac3659ce5
Swapnil <shaeer.musarrat@gmail.com>
2024-03-28 13:24:45 +0600

    admin panel dynamic dashboard page created

---
+++ resources/views/admin/layout/sidebar.blade.php
@@ -0,0 +1,32 @@
+<div class="main-sidebar">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand text-primary">
+            <a href="index.html" class="text-primary">Admin Panel</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html"></a>
+        </div>
+
+        <ul class="sidebar-menu">
+
+            <li class="active"><a class="nav-link text-primary" href="index.html"><i class="fas fa-hand-point-right"></i> <span>Dashboard</span></a></li>
+
+            <li class="nav-item dropdown active">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-hand-point-right text-primary"></i><span class="text-primary">Dropdown Items</span></a>
+                <ul class="dropdown-menu text-primary">
+                    <li class="active"><a class="nav-link text-primary" href=""><i class="fas fa-angle-right"></i> Item 1</a></li>
+                    <li class=""><a class="nav-link text-primary" href=""><i class="fas fa-angle-right"></i> Item 2</a></li>
+                </ul>
+            </li>
+
+            <li class=""><a class="nav-link text-primary" href="setting.html"><i class="fas fa-hand-point-right"></i> <span>Setting</span></a></li>
+
+            <li class=""><a class="nav-link text-primary" href="form.html"><i class="fas fa-hand-point-right"></i> <span>Form</span></a></li>
+
+            <li class=""><a class="nav-link text-primary" href="table.html"><i class="fas fa-hand-point-right"></i> <span>Table</span></a></li>
+
+            <li class=""><a class="nav-link text-primary" href="invoice.html"><i class="fas fa-hand-point-right"></i> <span>Invoice</span></a></li>
+
+        </ul>
+    </aside>
+</div>
