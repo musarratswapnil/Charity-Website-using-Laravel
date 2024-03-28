a68ea811151214c4bf6d1ba2c7087c38e94d0d42
Swapnil <shaeer.musarrat@gmail.com>
2024-03-28 15:04:37 +0600

    login and forget password page created

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\AdminHomeController;
+use App\Http\Controllers\Admin\AdminLoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/admin/home', [AdminHomeController::class, 'index'])->name('admin_home');
+
+Route::get('/admin/login', [AdminLoginController::class, 'index'])->name('admin_login');
+
+Route::get('/admin/forget-password', [AdminLoginController::class, 'forget_password'])->name('admin_forget_password');
